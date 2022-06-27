d862a2d6f8a425f4569b18ede8fa7b993cf6d4ba
DARK-Devil <98265982+malindunimsara@users.noreply.github.com>
2022-06-27 20:15:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ global.sp = '⭔' //A Charactor In Menu!
 
 
 global.ig = 'https://github.com/malindunimsara' //Github Account!
-global.footer = 'POWERD BY DARK-DEVIL' // DO NOT CHANGE!
+global.footer = '©_Kumuthu' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
