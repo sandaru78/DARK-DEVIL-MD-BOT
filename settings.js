37aacbcb62470cced2ddae1f3c91b9fe6e3459b0
DARK-Devil <98265982+malindunimsara@users.noreply.github.com>
2022-07-16 10:32:41 +0530

    👹DARK-DEVIL-BOT👺
    CREATED BY DARK DEVIL

--- settings.js
+++ settings.js
@@ -21,14 +21,14 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `➳ᴹᴿ᭄ᴰᴬᴿᴷ ᴰᴱᵛᴵᴸ❦🇱🇰
-My Creator is SASINDU MALSHAN` //Costomize Alive Message (type your message in `` )
+My Creator is 𝑴𝒂𝒍𝒊𝒏𝒅𝒖 𝑵𝒊𝒎𝒔𝒂𝒓𝒂` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+94754465051'] //Owner number in aive msg
-global.premium = ['+94754465051'] //Owner Number info
-global.ownernomer = '+94754465051' //Owner Number <<<
+global.owner = ['+94762817504'] //Owner number in aive msg
+global.premium = ['+94762817504'] //Owner Number info
+global.ownernomer = '+94762817504' //Owner Number <<<
 
-global.ownername = 'Sasindu malshan❦🇱🇰' //Owner Name
-global.botname = 'BLACK SHARK BOT✨' //Bot Name
+global.ownername = 'Maͥliͣnͫdu Nimsara❦🇱🇰' //Owner Name
+global.botname = '𝙳𝙰𝚁𝙺⚓𝙳𝙴𝚅𝙸𝙻-𝙱𝙾𝚃✨' //Bot Name
 
 global.button = '🖥️GITHUB〽️' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://github.com/malindunimsara' //Change Your Button's URL
@@ -36,7 +36,7 @@ global.btnurl = 'https://github.com/malindunimsara' //Change Your Button's URL
 global.region = 'srilanka' //Country Region
 
 global.packname = '👹𝗗𝗘𝗩𝗜𝗟 𝗦𝗧𝗜𝗖𝗞𝗘𝗥𝗦࿐' //Bot Pacage Name
-global.author = 'SASINDU MALSHAN❦🇱🇰' //Author Name
+global.author = 'Maͥliͣnͫdu Nimsara❦🇱🇰' //Author Name
 global.prefa = ['','❗','.','😂','👹','🎊'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
